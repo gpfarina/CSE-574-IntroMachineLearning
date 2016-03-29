c0f5cdf265df83c5db4eb267b4b43ba91a0a7874
gpietro <farinagianpietro@gmail.com>
2016-03-29 19:17:30 -0400

    second project

--- assignment2/base/script.py
+++ assignment2/base/script.py
@@ -18,7 +18,29 @@ def ldaLearn(X,y):
     # covmat - A single d x d learnt covariance matrix 
     
     # IMPLEMENT THIS METHOD
-    
+    Z=np.concatenate([X,y], axis=1)
+    Z1=Z[Z[:, 2] == 1, :]
+    m1=np.mean(Z1, axis=0)
+    m1=m1[:2]
+    Z2=Z[Z[:, 2] == 2, :]
+    m2=np.mean(Z2, axis=0)
+    m2=m2[:2]
+    Z3=Z[Z[:, 2] == 3, :]
+    m3=np.mean(Z3, axis=0)
+    m3=m3[:2]
+    Z4=Z[Z[:, 2] == 4, :]
+    m4=np.mean(Z4, axis=0)
+    m4=m4[:2]
+    Z5=Z[Z[:, 2] == 5, :]
+    m5=np.mean(Z5, axis=0)
+    m5=m5[:2]
+    means=np.array(m1,m2,m3,m4,m5)
+    covmat=np.cov(Z[:2])
+    prior[0]=np.float(Z1.shape[0])/np.float(Z.shape[0])
+    prior[1]=np.float(Z2.shape[0])/np.float(Z.shape[0])
+    prior[2]=np.float(Z3.shape[0])/np.float(Z.shape[0])
+    prior[3]=np.float(Z4.shape[0])/np.float(Z.shape[0])
+    prior[4]=np.float(Z5.shape[0])/np.float(Z.shape[0])
     return means,covmat
 
 def qdaLearn(X,y):
@@ -33,7 +55,9 @@ def qdaLearn(X,y):
     # IMPLEMENT THIS METHOD
     
     return means,covmats
-
+def delta(k, means, covmat, x, prior):
+    np.dot(np.dot(x.T, np.linalg.inv(covmat)), means[k-1])-0.5*np.dot(np.dot(means[k-1].T,np.linalg.inv(covmat)), means[k-1])+np.log(prior[k-1])
+    
 def ldaTest(means,covmat,Xtest,ytest):
     # Inputs
     # means, covmat - parameters of the LDA model
@@ -42,7 +66,10 @@ def ldaTest(means,covmat,Xtest,ytest):
     # Outputs
     # acc - A scalar accuracy value
     # ypred - N x 1 column vector indicating the predicted labels
-
+    for j in range(Xtest.shape[0]):
+        for i in range(1,6):
+            d[i-1]=delta(i, means, covmat, Xtest[j], prior[i-1])
+        ypred[j]=np.argmax(d)+1
     # IMPLEMENT THIS METHOD
     return acc,ypred
 
@@ -117,9 +144,11 @@ else:
     X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'),encoding = 'latin1')
 
 # LDA
+prior=np.zeros(5)
 means,covmat = ldaLearn(X,y)
 ldaacc = ldaTest(means,covmat,Xtest,ytest)
 print('LDA Accuracy = '+str(ldaacc))
+
 # QDA
 means,covmats = qdaLearn(X,y)
 qdaacc = qdaTest(means,covmats,Xtest,ytest)
